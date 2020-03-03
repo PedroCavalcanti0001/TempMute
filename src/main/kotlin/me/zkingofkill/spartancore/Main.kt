800a7df33b85873c14d9d7b9cfe02127021caab3
pedro <pedroeugeniocavalcanti@gmail.com>
2020-03-03 16:37:12 -0300

    fixes in Main.kt

--- src/main/kotlin/me/zkingofkill/spartancore/Main.kt
+++ src/main/kotlin/me/zkingofkill/spartancore/Main.kt
@@ -1,12 +1,22 @@
 package me.zkingofkill.spartancore
 
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 
 class Main : JavaPlugin() {
 
+    companion object {
+        lateinit var singleton: Main
+    }
+
     override fun onEnable() {
-        //code
+        singleton = this
+
+        config.options().copyHeader(true)
+        if (!File(dataFolder, "config.yml").exists()) {
+            saveDefaultConfig()
+        }
     }
 
 
